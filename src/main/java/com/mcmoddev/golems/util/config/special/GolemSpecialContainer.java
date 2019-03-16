de9de92d8b2886f3c91ef2da90bbc97ae4862ade
theglitch76 <19521552+TheGlitch76@users.noreply.github.com>
2019-03-16 18:22:56 -0500

    Actually add golem special code Fixes the .gitignore file preventing adding files to the config package

---
+++ src/main/java/com/mcmoddev/golems/util/config/special/GolemSpecialContainer.java
@@ -0,0 +1,44 @@
+package com.mcmoddev.golems.util.config.special;
+
+public class GolemSpecialContainer<K> {
+
+	public String name;
+	public K value;
+
+	protected String comment;
+
+	private GolemSpecialContainer(String lId, K defaultValue, String lComment) {
+		this.name = lId;
+		this.value = defaultValue;
+		this.comment = lComment;
+	}
+
+
+	/**
+	 * Fills the config options in a given builder
+	 */
+
+	@SuppressWarnings("unused")
+	public static class Builder<K> {
+		private final String id;
+		private final K defaultValue;
+
+		private String comment = "";
+
+		public Builder(String name, K defaultValue) {
+			this.id = name;
+			this.defaultValue = defaultValue;
+		}
+
+		public Builder setComment(String comment) {
+			this.comment = comment;
+			return this;
+		}
+
+		public GolemSpecialContainer build() {
+			return new GolemSpecialContainer<>(id, defaultValue, comment);
+		}
+
+
+	}
+}
