92494753239b95271f03c7d37da3aea66532f904
Skyler James <15920401+skyjay1@users.noreply.github.com>
2019-11-03 15:00:10 -0700

    Update EntityFurnaceGolem.java

--- src/main/java/com/golems/entity/EntityFurnaceGolem.java
+++ src/main/java/com/golems/entity/EntityFurnaceGolem.java
@@ -26,16 +26,18 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public final class EntityFurnaceGolem extends GolemBase {
 
 	private static final DataParameter<Integer> FUEL = EntityDataManager.createKey(EntityFurnaceGolem.class,
 			DataSerializers.VARINT);
 	private static final String KEY_FUEL = "FuelRemaining";
-	
+
 	private static final ResourceLocation LIT = makeTexture(ExtraGolems.MODID, GolemNames.FURNACE_GOLEM + "_lit");
 	private static final ResourceLocation UNLIT = makeTexture(ExtraGolems.MODID, GolemNames.FURNACE_GOLEM + "_unlit");
-	
+
 	public static final String FUEL_FACTOR = "Burn Time";
 	public static final int MAX_FUEL = 102400;
 	public final int fuelBurnFactor;
@@ -43,15 +45,16 @@ public final class EntityFurnaceGolem extends GolemBase {
 	public EntityFurnaceGolem(final World world) {
 		super(world);
 		fuelBurnFactor = Math.max(1, getConfig(this).getInt(FUEL_FACTOR));
+		this.setImmuneToFire(true);
 		this.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(1.0D);
 	}
-	
+
 	@Override
 	protected void entityInit() {
 		super.entityInit();
 		this.getDataManager().register(FUEL, Integer.valueOf(0));
 	}
-	
+
 	@Override
 	protected void initEntityAI() {
 		super.initEntityAI();
@@ -59,29 +62,28 @@ public final class EntityFurnaceGolem extends GolemBase {
 		this.tasks.addTask(1, new UseFuelGoal(this));
 		this.tasks.addTask(1, new EntityAITempt(this, 0.7D, Items.COAL, false));
 	}
-	
+
 	@Override
 	public void onLivingUpdate() {
 		super.onLivingUpdate();
-		if(this.world.isRemote && rand.nextInt(24) == 0) {
+		if (this.world.isRemote && rand.nextInt(24) == 0) {
 			// particle effects
 			final double pMotion = 0.03D;
-			world.spawnParticle(this.hasFuel() ? EnumParticleTypes.FLAME : EnumParticleTypes.SMOKE_NORMAL, 
+			world.spawnParticle(this.hasFuel() ? EnumParticleTypes.FLAME : EnumParticleTypes.SMOKE_NORMAL,
 					this.posX + world.rand.nextDouble() * 0.4D - 0.2D + this.motionX * 8,
-					this.posY + world.rand.nextDouble() * 0.5D + this.height / 3.0D, 
+					this.posY + world.rand.nextDouble() * 0.5D + this.height / 2.0D,
 					this.posZ + world.rand.nextDouble() * 0.4D - 0.2D + this.motionZ * 8,
-					world.rand.nextDouble() * pMotion - pMotion * 0.5D,
-					world.rand.nextDouble() * pMotion * 0.75D,
+					world.rand.nextDouble() * pMotion - pMotion * 0.5D, world.rand.nextDouble() * pMotion * 0.75D,
 					world.rand.nextDouble() * pMotion - pMotion * 0.5D);
 		}
 	}
-	
+
 	@Override
 	public void readEntityFromNBT(final NBTTagCompound tag) {
 		super.readEntityFromNBT(tag);
 		this.setFuel(tag.getInteger(KEY_FUEL));
 	}
-	
+
 	@Override
 	public void writeEntityToNBT(final NBTTagCompound tag) {
 		super.writeEntityToNBT(tag);
@@ -102,21 +104,21 @@ public final class EntityFurnaceGolem extends GolemBase {
 	public float getCollisionBorderSize() {
 		return this.hasFuel() ? super.getCollisionBorderSize() : 0.0F;
 	}
-	
+
 	@Override
 	protected boolean processInteract(final EntityPlayer player, final EnumHand hand) {
 		// allow player to add fuel to the golem by clicking on them with a fuel item
 		ItemStack stack = player.getHeldItem(hand);
 		int burnTime = getBurnAmount(stack);
-		if(burnTime > 0 && getFuel() < MAX_FUEL) {
-			if(player.isSneaking()) {
+		if (burnTime > 0 && getFuel() < MAX_FUEL) {
+			if (player.isSneaking()) {
 				// take entire ItemStack
 				this.addFuel(burnTime * stack.getCount());
 				stack = stack.getItem().getContainerItem(stack);
 			} else {
 				// take one item from ItemStack
 				this.addFuel(burnTime);
-				if(stack.getCount() > 1) {
+				if (stack.getCount() > 1) {
 					stack.shrink(1);
 				} else {
 					stack = stack.getItem().getContainerItem(stack);
@@ -125,23 +127,23 @@ public final class EntityFurnaceGolem extends GolemBase {
 			// update the player's held item
 			player.setHeldItem(hand, stack);
 			// add particles
-			ItemBedrockGolem.spawnParticles(this.world, this.posX, this.posY + this.height / 3.0D,
-					this.posZ, 0.03D, EnumParticleTypes.FLAME, 10);
+			ItemBedrockGolem.spawnParticles(this.world, this.posX, this.posY + this.height / 2.0D, this.posZ, 0.03D,
+					EnumParticleTypes.FLAME, 10);
 			return true;
 		}
-		
+
 		// allow player to remove burn time by using a water bucket
-		if(stack.getItem() == Items.WATER_BUCKET) {
+		if (stack.getItem() == Items.WATER_BUCKET) {
 			this.setFuel(0);
 			player.setHeldItem(hand, stack.getItem().getContainerItem(stack));
-			ItemBedrockGolem.spawnParticles(this.world, this.posX, this.posY + this.height / 3.0D,
-					this.posZ, 0.1D, EnumParticleTypes.SMOKE_LARGE, 15);
+			ItemBedrockGolem.spawnParticles(this.world, this.posX, this.posY + this.height / 2.0D, this.posZ, 0.1D,
+					EnumParticleTypes.SMOKE_LARGE, 15);
 			return true;
 		}
-		
+
 		return super.processInteract(player, hand);
 	}
-	
+
 	private static int getBurnAmount(final ItemStack i) {
 		return TileEntityFurnace.getItemBurnTime(i);
 	}
@@ -162,61 +164,79 @@ public final class EntityFurnaceGolem extends GolemBase {
 	public SoundEvent getGolemSound() {
 		return SoundEvents.BLOCK_STONE_STEP;
 	}
-	
+
 	public boolean hasFuel() {
 		return getFuel() > 0;
 	}
-	
+
 	/** @return the current fuel level as a number **/
 	public int getFuel() {
 		return this.getDataManager().get(FUEL).intValue();
 	}
-	
+
 	/** @return a number between 0.0 and 1.0 to indicate fuel level **/
 	public float getFuelPercentage() {
-		return (float)getFuel() / (float)MAX_FUEL;
+		return (float) getFuel() / (float) MAX_FUEL;
 	}
-	
+
 	public void setFuel(final int fuel) {
-		if(getFuel() != fuel) {
+		if (getFuel() != fuel) {
 			this.getDataManager().set(FUEL, Integer.valueOf(fuel));
 		}
 	}
-	
+
 	public void addFuel(final int toAdd) {
-		if(toAdd != 0) {
-			this.getDataManager().set(FUEL, getFuel() + toAdd);			
+		if (toAdd != 0) {
+			this.getDataManager().set(FUEL, getFuel() + toAdd);
 		}
 	}
-	
+
+	/**
+	 * Sets the head's yaw rotation of the entity.
+	 */
+	@Override
+	public void setRotationYawHead(float rotation) {
+		if (hasFuel()) {
+			super.setRotationYawHead(rotation);
+		}
+	}
+
+	@SideOnly(Side.CLIENT)
+	public void turn(float yaw, float pitch) {
+		if (this.hasFuel()) {
+			super.turn(yaw, pitch);
+		}
+	}
+
 	@Override
 	public List<String> addSpecialDesc(final List<String> list) {
 		// add special
 		list.add(TextFormatting.GRAY + trans("entitytip.use_fuel"));
 		// add fuel amount to the tooltip
 		final int fuel = this.getFuel();
-		if(fuel > 0) {
-			final int percentFuel = (int)Math.ceil(this.getFuelPercentage() * 100F);
+		if (fuel > 0) {
+			final int percentFuel = (int) Math.ceil(this.getFuelPercentage() * 100F);
 			final TextFormatting color;
-			if(percentFuel < 6) {
+			if (percentFuel < 6) {
 				color = TextFormatting.RED;
-			} else if(percentFuel < 16) {
+			} else if (percentFuel < 16) {
 				color = TextFormatting.YELLOW;
 			} else {
 				color = TextFormatting.WHITE;
 			}
 			// if sneaking, show exact value, otherwise show percentage value
-			final String fuelString = GuiScreen.isShiftKeyDown() ? Integer.toString(fuel) : (Integer.toString(percentFuel) + "%");
+			final String fuelString = GuiScreen.isShiftKeyDown() ? Integer.toString(fuel)
+					: (Integer.toString(percentFuel) + "%");
 			// actually add the description
 			list.add(TextFormatting.GRAY + trans("entitytip.fuel") + ": " + color + fuelString);
 		}
 		return list;
 	}
-	
+
 	class UseFuelGoal extends EntityAIBase {
-		
+
 		private final EntityFurnaceGolem golem;
-		
+
 		protected UseFuelGoal(final EntityFurnaceGolem entity) {
 			super();
 			golem = entity;
@@ -227,44 +247,43 @@ public final class EntityFurnaceGolem extends GolemBase {
 			// only uses fuel every X ticks
 			return golem.isServerWorld() && golem.getFuel() > 0 && golem.ticksExisted % golem.fuelBurnFactor == 0;
 		}
-		
+
 		@Override
 		public boolean shouldContinueExecuting() {
 			return false;
 		}
-		
+
 		@Override
 		public void startExecuting() {
 			golem.addFuel(-1);
 		}
 	}
-	
+
 	class InertGoal extends EntityAIBase {
-		
+
 		private final EntityFurnaceGolem golem;
-		
+
 		protected InertGoal(final EntityFurnaceGolem entity) {
 			super();
 			this.setMutexBits(7);
 			golem = entity;
 		}
-		
+
 		@Override
 		public boolean shouldExecute() {
 			return !golem.hasFuel();
 		}
-		
+
 		@Override
 		public boolean shouldContinueExecuting() {
-			golem.newPosRotationIncrements = 3;
 			return false;
 		}
-		
+
 		@Override
 		public void startExecuting() {
 			updateTask();
 		}
-		
+
 		@Override
 		public void updateTask() {
 			// freeze the golem and ai tasks
@@ -277,14 +296,13 @@ public final class EntityFurnaceGolem extends GolemBase {
 			golem.setAttackTarget(null);
 			golem.setRevengeTarget(null);
 			golem.getNavigator().clearPath();
-			golem.rotationPitch = golem.prevRotationPitch;
-			golem.rotationYaw = golem.prevRotationYaw;
+			golem.setRotation(golem.prevRotationYaw, -15F);
 			// set looking down
 			final double lookX = golem.getLookVec().x;
-			final double lookY = Math.toRadians(-75D);
+			final double lookY = Math.toRadians(-15D);
 			final double lookZ = golem.getLookVec().z;
-			golem.getLookHelper().setLookPosition(lookX, lookY, lookZ, golem.getHorizontalFaceSpeed(), golem.getVerticalFaceSpeed());			
-			golem.newPosRotationIncrements = 100;
+			golem.getLookHelper().setLookPosition(lookX, lookY, lookZ, golem.getHorizontalFaceSpeed(),
+					golem.getVerticalFaceSpeed());
 
 		}
 	}
