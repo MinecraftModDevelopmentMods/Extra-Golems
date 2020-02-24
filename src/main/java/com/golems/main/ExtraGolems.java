53ff6ed73df858ae9833f23e0f3d1af804c2f8ad
Skyler James <15920401+skyjay1@users.noreply.github.com>
2020-02-23 17:24:24 -0700

    Update version no.

--- src/main/java/com/golems/main/ExtraGolems.java
+++ src/main/java/com/golems/main/ExtraGolems.java
@@ -17,7 +17,7 @@ public class ExtraGolems {
 
   public static final String MODID = "golems";
   protected static final String NAME = "Extra Golems";
-  protected static final String VERSION = "7.1.8";
+  protected static final String VERSION = "7.1.9";
 
   @SidedProxy(clientSide = "com." + MODID + ".proxies.ClientProxy", serverSide = "com." + MODID
       + ".proxies.CommonProxy")
